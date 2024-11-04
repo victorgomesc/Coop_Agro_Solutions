81034b608a24523c0cb5ea4e3a535979ebae169c
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-11-03 22:08:52 -0300

    feat: create page equipe

---
+++ app/equipe/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+
+
+export default function Equipe() {
+    return (
+        <div className="items-center justify-items-center min-h-screen sm:p-10">
+            <Header />
+            <Navbar /> 
+        </div>    
+    )
+}
+   
+    
