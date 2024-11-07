4cbe6e05b7adfbd1c45a45e095e81d64e6419356
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-11-07 16:18:49 -0300

    feat: implement description text

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ import Sidebar from "@/components/Sidebar";
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <div className="flex flex-col items-center justify-center bg-zinc-200">
+      <div className="flex items-center justify-center bg-zinc-200">
         <Header />
         <Navbar />
       </div>
       
       <div className="flex">
         <Sidebar />
-        <div className="flex flex-col">
+        <div className="flex">
           <Baner />
           <BusinessDescription />
         </div>
--- components/BusinessDescription.tsx
+++ components/BusinessDescription.tsx
@@ -5,7 +5,10 @@ const BusinessDescription = () => {
     <div className="p-10">
         <section>
           <p>
-            A agro coop solutions e uma empresa voltada para o setor cooperativista rural 
+            A agro coop solutions e uma empresa voltada para o setor cooperativista rural, que tem como principal ambição
+            ajudar as coperativas rurais a se organizar de forma inovadora, estamos localizados no sertão central
+            cerense mais precisamente na cidade de quixada, somos estudantes da universidade federal do ceara em cursos 
+            de tecnologia
           </p>
         </section>
     </div>
