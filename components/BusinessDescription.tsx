93d0ea6a09765ab5e5a47236a2f88bf4eacb8581
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-11-09 20:25:39 -0300

    test: text area

--- components/BusinessDescription.tsx
+++ components/BusinessDescription.tsx
@@ -8,7 +8,7 @@ const BusinessDescription = () => {
             A agro coop solutions e uma empresa voltada para o setor cooperativista rural, que tem como principal ambição
             ajudar as coperativas rurais a se organizar de forma inovadora, estamos localizados no sertão central
             cerense mais precisamente na cidade de quixada, somos estudantes da universidade federal do ceara em cursos 
-            de tecnologia
+            de tecnologia Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam assumenda delectus minus aut, alias soluta quo. Quaerat nostrum ipsum eveniet sapiente nobis dolorem perspiciatis possimus, ullam porro ex. Esse, rem.
           </p>
         </section>
     </div>
