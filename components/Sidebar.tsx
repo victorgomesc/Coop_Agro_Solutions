946ea0a4465522f1d718edcc6a8ebcd64021b2c1
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-11-19 23:06:39 -0300

    fix: to correct asside

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,6 +9,11 @@ const Sidebar = () => {
             <h2 className="mt-6">O que oferecemos?</h2>
             <h2 className="mt-6">Descrição do produto?</h2>
             <h2 className="mt-6">O que nos diferencia?</h2>
+            <h1 className="text-xl font-bold">Agro Coop Solutions</h1>
+            <h2 className="mt-6">Quem somos?</h2>
+            <h2 className="mt-6">O que oferecemos?</h2>
+            <h2 className="mt-6">Descrição do produto?</h2>
+            <h2 className="mt-6">O que nos diferencia?</h2>
         </div>
       
     </section>
